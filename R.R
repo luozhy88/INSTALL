4eb724cfebb84d835ca8375dca5250deff7e5723
luozhy88 <94740863+luozhy88@users.noreply.github.com>
2023-11-03 14:00:17 +0800

    Update R.R

--- R.R
+++ R.R
@@ -3,3 +3,18 @@ if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
 BiocManager::install("SingleCellExperiment")
+
+if (!requireNamespace("remotes", quietly = TRUE)) {
+  install.packages("remotes")
+}
+remotes::install_github("mojaveazure/seurat-disk")
+
+remotes::install_github("satijalab/seurat", "seurat5", quiet = TRUE)
+
+remotes::install_github("satijalab/seurat-data", "seurat5", quiet = TRUE)
+remotes::install_github("mojaveazure/seurat-object", "seurat5", quiet = TRUE)
+remotes::install_github("satijalab/azimuth", "seurat5", quiet = TRUE)
+remotes::install_github("satijalab/seurat-wrappers", "seurat5", quiet = TRUE)
+remotes::install_github("stuart-lab/signac", "seurat5", quiet = TRUE)
+
+remotes::install_github("bnprks/BPCells", quiet = TRUE)
